7e82db2691883e6d0c3f7464908f0a45574446f9
Michael Albuquerque <michael.albqq@gmail.com>
2024-09-28 14:35:33 -0300

    fix: ensuring that positions stay inbounds

--- PSO/main.py
+++ PSO/main.py
@@ -14,12 +14,13 @@ def generate_initial_particles(position_left_bound, position_right_bound, dimens
     population = []
     for i in range(population_size):
         for j in range(dimension):
+            #Mudança que o senhor pediu sobre velocidade
             particle = Particle([random.uniform(position_left_bound, position_right_bound) for i in range(dimension)], [random.uniform(position_left_bound, position_right_bound) for i in range(dimension)])
         population.append(particle)
     
     return population
 
-def update_particles_position(population, general_best_position, intertiaStrategy):
+def update_particles_position(population, left_bound, right_bound, general_best_position, intertiaStrategy):
     c1 = 1.5
     c2 = 1.5
 
@@ -34,8 +35,17 @@ def update_particles_position(population, general_best_position, intertiaStrateg
                            c1*random.random()*(particle.best_position[axis_index] - particle.position[axis_index]) + \
                            c2*random.random()*(general_best_position[axis_index] - particle.position[axis_index])
 
-            particle.position[axis_index] = particle.position[axis_index] + new_axis_velocity 
+            curr_new_axis_position = particle.position[axis_index] + new_axis_velocity 
+            curr_new_axis_position = ensure_axis_position_is_inbounds(left_bound, right_bound, curr_new_axis_position) #Mudança que o senhor pediu sobre manter as posições nos limites
+
+            particle.position[axis_index] = curr_new_axis_position
             particle.velocity[axis_index] = new_axis_velocity
+
+def ensure_axis_position_is_inbounds(left_bound, right_bound, position):
+    if position > 0:
+        return min(right_bound, position)
+    
+    return max(left_bound, position)
 
 def find_best_particle_from_iteration(population, evaluator):
     best_particle = None
@@ -59,8 +69,10 @@ def find_best_particle_from_iteration(population, evaluator):
 def main():
     
     NUMBER_ITERATIONS = 1000
+    LEFT_BOUND = -100
+    RIGHT_BOUND = 100
 
-    initial_population = generate_initial_particles(-1, 1, 2, 4)
+    initial_population = generate_initial_particles(-100, 100, 30, 1000)
     evaluator = SphereEvaluator()
     intertiaStrategy = LinearDescentInertia(0.2, 1, NUMBER_ITERATIONS)
     best_particle = initial_population[0]
@@ -69,10 +81,10 @@ def main():
     for iteration in range(NUMBER_ITERATIONS):
         best_particle_from_iteration = find_best_particle_from_iteration(initial_population, evaluator)
 
-        if best_particle_from_iteration.fitness < best_particle.fitness:
+        if best_particle_from_iteration.fitness < best_particle.fitness: #Mudança que o senhor pediu sobre a melhor particula
             best_particle = best_particle_from_iteration
 
-        update_particles_position(initial_population, best_particle.position, intertiaStrategy)
+        update_particles_position(initial_population, LEFT_BOUND, RIGHT_BOUND, best_particle.position, intertiaStrategy)
 
         print("================================================================")
         print(f'Iteration: {iteration + 1}# Best score: {best_particle_from_iteration.fitness}.')
