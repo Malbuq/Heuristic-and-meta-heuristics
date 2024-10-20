3dc0f8b8924120491e7d5845e0a3fa06bc033a05
Michael Albuquerque <michael.albqq@gmail.com>
2024-10-20 10:35:01 -0300

    ABC algorithm done

--- ABC/main.py
+++ ABC/main.py
@@ -16,12 +16,13 @@ class Bee():
         neighborSolutionDimensionValue = neighborSolution.position[randomDimensionIndex]
 
         candidateSolution = copy.deepcopy(self.solution)
-        candidateSolution.position[randomDimensionIndex] =  currentSolutionDimensionValue + random.uniform(-1, 1)*(currentSolutionDimensionValue - neighborSolutionDimensionValue)
+        newValue = currentSolutionDimensionValue + random.uniform(-1, 1)*(currentSolutionDimensionValue - neighborSolutionDimensionValue)
+        candidateSolution.position[randomDimensionIndex] =  newValue
 
         candidateSolution.fitness = fitnessEvaluator.calculateSolutionFitness(candidateSolution)
 
         if candidateSolution.fitness < self.solution.fitness:
-            self.solution = candidateSolution
+            self.solution.position[randomDimensionIndex] = newValue
             self.solution.improvAttemptCount = 0
             return
 
@@ -48,36 +49,76 @@ def initializeEmployeedBees(solutions):
 
     return employeedBees
 
+def initializeOnlookerBees(solutions):
+    onlookerBees = [Bee(None) for _ in range(len(solutions))]
+
+    for index in range(len(onlookerBees)):
+        onlookerBee = onlookerBees[index]
+
+        populationFitness = sum([solution.fitness for solution in solutions])
+        
+        solutionsWeights = [1 - (solution.fitness/populationFitness) for solution in solutions]
+
+        onlookerBee.solution = random.choices(solutions, solutionsWeights, k=1)[0]
+
+    return onlookerBees
+
 def employeedBeesExploration(employeedBees, solutions, fitnessEvaluator):
     for beeIndex in range(len(employeedBees)):
         employeedBees[beeIndex].explore(solutions, fitnessEvaluator)
 
 def onlookerBeesExploration(onlookerBees, solutions, fitnessEvaluator):
-    pass
+    for beeIndex in range(len(onlookerBees)):
+        onlookerBees[beeIndex].explore(solutions, fitnessEvaluator)
 
 def scoutBeesExploration(bees, maxImprovTries):
     for index in range(len(bees)):
         currentPossibleScout = bees[index]
-        
+
         if currentPossibleScout.solution.improvAttemptCount >= maxImprovTries:
             newRandomSolution = Solution(currentPossibleScout.solution.leftDomainBound, currentPossibleScout.solution.rightDomainBound, currentPossibleScout.solution.dimensionSize)
             currentPossibleScout.solution = newRandomSolution
 
+def run(colonySize, leftDomainBound, rightDomainBound, searchSpaceDimension, fitnessEvaluator, numberIterations, maxImprovTries):
+    solutions = initializeSolutions(leftDomainBound, rightDomainBound, searchSpaceDimension, colonySize)
+    fitnessEvaluator.evaluateSolutions(solutions)
 
+    employeedBees = initializeEmployeedBees(solutions)
+    onlookerBees = initializeOnlookerBees(solutions)
+
+    for iteration in range(numberIterations):
+        fitnessEvaluator.evaluateSolutions(solutions)
+
+        print(selectBestSolutionFromIteration(solutions).fitness)
+
+        employeedBeesExploration(employeedBees, solutions, fitnessEvaluator)
+        onlookerBeesExploration(onlookerBees, solutions, fitnessEvaluator)
+
+        scoutBees = employeedBees + onlookerBees
+        scoutBeesExploration(scoutBees, maxImprovTries)
+
+    return solutions
+
+def selectBestSolutionFromIteration(solutions):
+    bestSolution = None
+    
+    for solution in solutions:
+        if bestSolution is None or solution.fitness < bestSolution.fitness:
+            bestSolution = solution
+    
+    return bestSolution
 
 def main():
-    s1 = Solution(-10, 10, 3)
-    s2 = Solution(-10, 10, 3)
-    s3 = Solution(-10, 10, 3)
+    COLONY_SIZE = 100
+    NUMBER_ITERATIONS = 1000
+    LEFT_BOUND = -100
+    RIGHT_BOUND = 100
+    SEARCH_SPACE_DIMENSIONS = 30
+    MAX_IMPROV_TRIES = COLONY_SIZE*SEARCH_SPACE_DIMENSIONS #Isso aqui é MUITO importante pro quao rapido o algoritimo converge
 
-    solutions = [s1,s2,s3]
+    fitnessEvaluator = SphereEvaluator()
 
-    SphereEvaluator().evaluateSolutions(solutions)
+    solutions = run(COLONY_SIZE, LEFT_BOUND, RIGHT_BOUND, SEARCH_SPACE_DIMENSIONS, fitnessEvaluator, NUMBER_ITERATIONS, MAX_IMPROV_TRIES)
 
-    empBee = Bee(s1)
-
-    for _ in range(5):
-        empBee.explore(solutions, SphereEvaluator())
-        
 
 main()
